bf4a5ce203165ffcfa6fbde8824c3b05e7f46c42
durgaprasad445 <77887008+durgaprasad445@users.noreply.github.com>
2021-07-22 20:10:25 +0530

    Update Assignment-2.py

--- Assignment-2.py
+++ Assignment-2.py
@@ -1,7 +1,8 @@
 import requests 
+import json
 list_req = []
 for x in range(1,6):
   r = requests.get(url = "https://jsonplaceholder.typicode.com/todos/" + str(x))
   list_req.append(r)
-for i in list(list_req):
+for i in list_req:
     print(i.json())
