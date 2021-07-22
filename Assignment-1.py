36f303573056e6b808d29e81d8c7fb7586807f0a
durgaprasad445 <77887008+durgaprasad445@users.noreply.github.com>
2021-07-22 08:53:49 +0530

    Create Assignment-1.py
    Updated Readme

---
+++ Assignment-1.py
@@ -0,0 +1,11 @@
+n = int(input('Enter Any Value: '))
+m = input('Please Enter Your Name: ')
+for i in range(1,n):
+    if i % 3 and i % 5 == 0:
+        print(m)
+    elif i % 3 == 0:
+        print('3n')
+    elif i % 5 == 0:
+        print('5n')
+    else:
+        print(False)
---
+++ README.md
@@ -0,0 +1,4 @@
+# Python Programs
+ E-Bibil Programming Assingment
+This is a Local Repo
+Contains Assignment Programs in Python Language
